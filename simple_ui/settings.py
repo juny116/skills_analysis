6574733a492dc044e22b35cc04351e2f044dba53
juny116 <juny116@gmail.com>
2022-10-25 15:50:49 +0900

    move key to env

--- simple_ui/settings.py
+++ simple_ui/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gp+wv1a($47#wr7aig-w*biao#@fz_!r^395rie7(xm@yp0_x^'
+SECRET_KEY = os.environ["SKILLS_SECRET_KEY"]
+# SECRET_KEY = 'django-insecure-gp+wv1a($47#wr7aig-w*biao#@fz_!r^395rie7(xm@yp0_x^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
