dc4a14a42f3936a7e6e6c0e48dcc71c9b7bd0e6e
juny116 <juny116@gmail.com>
2022-12-18 17:54:52 +0900

    update ui

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,15 @@ class WeightForm(forms.ModelForm):
     class Meta:
         model = WeightSetup
         fields = ("name", "description", "class_1", "class_2", "abilities_concat", "objective", "abilities_avg")
-
+        widgets = {
+            'name': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'description': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'class_1': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'class_2': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'abilities_concat': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'objective': forms.NumberInput(attrs={'style': 'width: 80px'}),
+            'abilities_avg': forms.NumberInput(attrs={'style': 'width: 80px'}),
+        }
 
 class SearchForm(forms.Form):
     job_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'style': 'width: 200px', 'id':'tags'}))
@@ -22,3 +30,6 @@ class MaskStringForm(forms.ModelForm):
     class Meta:
         model = MaskString
         fields = ("mask", )
+        widgets = {
+            'mask': forms.TextInput(attrs={'style': 'width: 100px'}),
+        }
